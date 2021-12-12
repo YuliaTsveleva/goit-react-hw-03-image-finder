5d075e6a05f49984230a89fd68b33ff8e6b1abbd
YuliaTsveleva <tsveleva.yulia@gmail.com>
2021-12-12 20:24:22 +0200

    fixed api

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,8 @@
+const API_KEY = '24048830-4cc4486dcdd2cd17ebea2a9c8';
+
 function fetchImages(newQuery, page) {
   return fetch(
-    `https://pixabay.com/api/?q=${newQuery}&page=1&key=24048830-4cc4486dcdd2cd17ebea2a9c8&image_type=photo&orientation=horizontal&page=${page}&per_page=12`,
+    `https://pixabay.com/api/?q=${newQuery}&page=${page}&&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
   )
     .then(response => {
       if (response.ok) {
